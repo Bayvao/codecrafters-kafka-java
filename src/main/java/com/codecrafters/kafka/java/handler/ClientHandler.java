81a5380f890cd5d0162bec38604f0ebd87aa6a23
Bayvao <vermabayvao@gmail.com>
2024-11-02 01:18:52 +0530

    refactored code

--- src/main/java/com/codecrafters/kafka/java/handler/ClientHandler.java
+++ src/main/java/com/codecrafters/kafka/java/handler/ClientHandler.java
@@ -10,6 +10,11 @@ import java.util.Arrays;
 
 public class ClientHandler {
 
+    private static final int UNSUPPORTED_VERSION_ERROR_CODE = 35;
+    private static final int NO_ERROR_CODE = 0;
+    private static final int API_VERSIONS_KEY = 18;
+    private static final int SUPPORTED_API_VERSION_MIN = 0;
+    private static final int SUPPORTED_API_VERSION_MAX = 4;
     private final Socket clientSocket;
 
     public ClientHandler(Socket clientSocket) {
@@ -17,15 +22,11 @@ public class ClientHandler {
     }
 
     public void handleClients() {
-        BufferedInputStream bufferedInputStream = null;
-        OutputStream outputStream = null;
-        ByteArrayOutputStream bos = null;
         while (!clientSocket.isClosed()) {
 
             try {
-                bufferedInputStream = new BufferedInputStream(clientSocket.getInputStream());
-                outputStream = clientSocket.getOutputStream();
-                bos = new ByteArrayOutputStream();
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(clientSocket.getInputStream());
+                OutputStream outputStream = clientSocket.getOutputStream();
 
                 // Request Header v2 => request_api_key request_api_version correlation_id client_id TAG_BUFFER
                 //      request_api_key => INT16
@@ -41,44 +42,53 @@ public class ClientHandler {
                 // client_id nullable string
                 // tagged fields nullable
 
-
                 // response
-                bos.write(correlationId);
                 if (apiVersion < 0 || apiVersion > 4) {
-                    bos.write(new byte[]{0, 35});
+                   sendErrorResponse(outputStream, correlationId);
                 } else {
-
                     // error code 16 bit
                     // api_key => INT16
                     // min_version => INT16
                     // max_version => INT16
                     // throttle_time_ms => INT32
-                    bos.write(new byte[]{0, 0});  // error code
-                    bos.write(2);               // array size + 1
-                    bos.write(new byte[]{0, 18}); // api_key
-                    bos.write(new byte[]{0, 3});  // min version
-                    bos.write(new byte[]{0, 4});  // max version
-                    bos.write(0);               // tagged fields
-                    bos.write(new byte[]{0, 0, 0, 0}); // throttle time
-                    // All requests and responses will end with a tagged field buffer.  If
-                    // there are no tagged fields, this will only be a single zero byte.
-                    bos.write(0); // tagged fields
+                    sendAPIVersionsResponse(outputStream, correlationId);
                 }
-
-                int size = bos.size();
-                byte[] sizeBytes = ByteBuffer.allocate(4).putInt(size).array();
-                var response = bos.toByteArray();
-                System.out.println(Arrays.toString(sizeBytes));
-                System.out.println(Arrays.toString(response));
-                outputStream.write(sizeBytes);
-                outputStream.write(response);
-                outputStream.flush();
 
             } catch (IOException ex) {
                 System.out.println("Exception occurred: " + ex.getMessage());
 
             }
         }
+    }
+
+    private void sendErrorResponse(OutputStream out, byte[] correlationId) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        bos.write(correlationId); // Correlation ID
+        bos.write(new byte[] {0, (byte)UNSUPPORTED_VERSION_ERROR_CODE}); // Error code (35)
+        int size = bos.size();
+        out.write(ByteBuffer.allocate(4).putInt(size).array()); // Message size
+        out.write(bos.toByteArray());                           // Payload
+        out.flush();
+    }
+
+    private void sendAPIVersionsResponse(OutputStream out, byte[] correlationId) throws IOException {
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        bos.write(correlationId); // Correlation ID
+        bos.write(new byte[] {0, (byte)NO_ERROR_CODE});            // No error
+        bos.write(2); // Number of API keys
+        bos.write(new byte[] {0, (byte)API_VERSIONS_KEY}); // API key (API_VERSIONS_KEY)
+        bos.write(new byte[] {0, (byte)SUPPORTED_API_VERSION_MIN}); // Min version
+        bos.write(new byte[] {0, (byte)SUPPORTED_API_VERSION_MAX}); // Max version
+        bos.write(0);
+        bos.write(new byte[] {0, 0, 0, 0}); // Throttle time
+        bos.write(0);                       // Tagged fields end byte
+        int size = bos.size(); // tagged fields
+        out.write(ByteBuffer.allocate(4).putInt(size).array()); // Message size
+        out.write(bos.toByteArray());                           // Payload
+        out.flush();
     }
 
 }
