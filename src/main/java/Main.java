3758846a61d018872f0c6c147bd815d615c038c8
Bayvao <vermabayvao@gmail.com>
2024-11-01 23:16:41 +0530

    reading and parsing api version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,23 +15,18 @@ public class Main {
 
             InputStream inputStream = clientSocket.getInputStream();
             OutputStream outputStream = clientSocket.getOutputStream();
-            byte[] buffer = new byte[1024];
 
-            if (inputStream.read(buffer) != -1) {
-                byte[] length = inputStream.readNBytes(4);
-                byte[] apiKey = inputStream.readNBytes(2);
-                byte[] apiVersion = inputStream.readNBytes(2);
-                short shortApiVersion = ByteBuffer.wrap(apiVersion).getShort();
-                byte[] correlationId = inputStream.readNBytes(4);
+            byte[] length = inputStream.readNBytes(4);
+            byte[] apiKey = inputStream.readNBytes(2);
+            byte[] apiVersion = inputStream.readNBytes(2);
+            short shortApiVersion = ByteBuffer.wrap(apiVersion).getShort();
+            byte[] correlationId = inputStream.readNBytes(4);
 
-                outputStream.write(length);
-                outputStream.write(correlationId);
+            outputStream.write(length);
+            outputStream.write(correlationId);
 
-                if (shortApiVersion < 0 || shortApiVersion > 4) {
-                    outputStream.write(new byte[] {0, 35});
-                }
-            } else {
-                System.out.println("Nothing to read from input stream");
+            if (shortApiVersion < 0 || shortApiVersion > 4) {
+                outputStream.write(new byte[] {0, 35});
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
