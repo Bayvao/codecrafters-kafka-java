7e6388bca2cc49af11304ff4a9fc180a7d8d8448
Bayvao <vermabayvao@gmail.com>
2024-11-01 23:14:51 +0530

    reading and parsing api version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class Main {
     public static void main(String[] args){
@@ -17,10 +18,18 @@ public class Main {
             byte[] buffer = new byte[1024];
 
             if (inputStream.read(buffer) != -1) {
-                byte[] output = new byte[] {0, 0, 0, 0, 0, 0, 0, 0};
-                System.arraycopy(buffer, 8, output, 4, 4);
-                System.out.println("Input from src: " + new String(buffer));
-                outputStream.write(output);
+                byte[] length = inputStream.readNBytes(4);
+                byte[] apiKey = inputStream.readNBytes(2);
+                byte[] apiVersion = inputStream.readNBytes(2);
+                short shortApiVersion = ByteBuffer.wrap(apiVersion).getShort();
+                byte[] correlationId = inputStream.readNBytes(4);
+
+                outputStream.write(length);
+                outputStream.write(correlationId);
+
+                if (shortApiVersion < 0 || shortApiVersion > 4) {
+                    outputStream.write(new byte[] {0, 35});
+                }
             } else {
                 System.out.println("Nothing to read from input stream");
             }
