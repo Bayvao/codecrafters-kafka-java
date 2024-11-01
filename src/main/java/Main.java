8a7ca57dd1282397c6768ba9ae8622d765d22915
Bayvao <vermabayvao@gmail.com>
2024-11-01 23:02:08 +0530

    parse correlation id from input and send it back in the response.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    public static void main(String[] args){
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-     int port = 9092;
-    BufferedReader bufferedReader;
-    OutputStream outputStream;
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+            byte[] buffer = new byte[1024];
 
-     try {
-       serverSocket = new ServerSocket(port);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept();
-
-       bufferedReader = new BufferedReader(
-               new InputStreamReader(clientSocket.getInputStream()));
-
-       outputStream = clientSocket.getOutputStream();
-       outputStream.write(new byte[] {0, 1, 2, 3});
-       outputStream.write(new byte[] {0, 0, 0, 7});
-
-       System.out.println("Got from cli: " + bufferedReader.readLine());
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     } finally {
-       try {
-         if (clientSocket != null) {
-           clientSocket.close();
-         }
-         if (serverSocket != null) {
-           serverSocket.close();
-         }
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       }
-     }
-  }
+            if (inputStream.read(buffer) != -1) {
+                byte[] output = new byte[] {0, 0, 0, 0, 0, 0, 0, 0};
+                System.arraycopy(buffer, 8, output, 4, 4);
+                System.out.println("Input from src: " + new String(buffer));
+                outputStream.write(output);
+            } else {
+                System.out.println("Nothing to read from input stream");
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
 }
